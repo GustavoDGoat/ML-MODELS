271406a8c8033706631210eb89a4e15496e5d966
Gustavo <gustavodgoat@gmail.com>
2025-10-04 16:55:55 +0100

    Update Chuka.tsx

--- Chuka.tsx
+++ Chuka.tsx
@@ -1,1 +1,2 @@
 https://www.kaggle.com/code/ryanholbrook/what-is-feature-engineering/data?select=DataDocumentation.txt
+https://www.youtube.com/watch?v=fiz1ORTBGpY&list=PLfFghEzKVmjvuSA67LszN1dZ-Dd_pkus6
